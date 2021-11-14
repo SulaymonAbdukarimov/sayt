35fbbdd74bcaa393ef14da2368629351a743e795
SulaymonAbdukarimov <sulaymonabdukarimov2@gmail.com>
2021-11-14 09:11:10 +0500

    pagination

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,6 +4,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+
 const genres = [
   { id: 28, name: "Action" },
   { id: 12, name: "Adventure" },
@@ -29,7 +30,18 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const tagsEl = document.getElementById("tags");
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
+
+let currentPage = 1;
+let nextPage = 2;
+let prevPage = 3;
+let lastUrl = "";
+let totalPages = 2;
 let selectedGenre = [];
+
 setGence();
 function setGence() {
   tagsEl.innerHTML = "";
@@ -52,7 +64,6 @@ function setGence() {
           selectedGenre.push(genre.id);
         }
       }
-      console.log(selectedGenre);
       getMovies(API_URL + "&with_genres=" + encodeURI(selectedGenre.join(",")));
       highlightSelection();
     });
@@ -92,12 +103,32 @@ function clearBtn() {
 getMovies(API_URL);
 
 function getMovies(url) {
+  lastUrl = url;
+
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results);
+      //   console.log(data.results);
+
       if (data.results.length !== 0) {
         showMovies(data.results);
+        currentPage = data.page;
+        nextPage = currentPage + 1;
+        prevPage = currentPage - 1;
+        totalPages = data.total_pages;
+
+        current.innerHTML = currentPage;
+        if (currentPage <= 1) {
+          prev.classList.add("disabled");
+          next.classList.remove("disabled");
+        } else if (currentPage >= totalPages) {
+          prev.classList.remove("disabled");
+          next.classList.add("disabled");
+        } else {
+          prev.classList.remove("disabled");
+          next.classList.remove("disabled");
+        }
+        // tagsEl.scrollIntoView({ behavior: "smooth" });
       } else {
         main.innerHTML = `<h1 class="no-results" >No Results Found </h1>`;
       }
@@ -115,7 +146,7 @@ function showMovies(data) {
           poster_path
             ? IMG_URL + poster_path
             : "https://via.placeholder.com/1080x1580"
-        }" alt="${title}">
+        }" class="helloworld" alt="${title}">
 
         <div class="movie-info">
             <h3>${title}</h3>
@@ -151,3 +182,33 @@ form.addEventListener("submit", (e) => {
     getMovies(API_URL);
   }
 });
+
+prev.addEventListener("click", () => {
+  if (prevPage > 0) {
+    pageCall(prevPage);
+  }
+});
+
+next.addEventListener("click", () => {
+  if (nextPage <= totalPages) {
+    pageCall(nextPage);
+  }
+});
+
+function pageCall(page) {
+  let urlSplit = lastUrl.split("?");
+  let queryParams = urlSplit[1].split("&");
+  let key = queryParams[queryParams.length - 1].split("=");
+
+  if (key[0] != "page") {
+    let url = lastUrl + "&page=" + page;
+    getMovies(url);
+  } else {
+    key[1] = page.toString();
+    let a = key.join("=");
+    queryParams[queryParams.length - 1] = a;
+    let b = queryParams.join("&");
+    let url = urlSplit[0] + "?" + b;
+    getMovies(url);
+  }
+}
